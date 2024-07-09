ed6e96ef46c2eab2d17f57cd50b8fd84d54c1b6f
somilisa <somilisa1010@gmail.com>
2024-07-10 00:19:37 +0100

    display images

--- src/data.js
+++ src/data.js
@@ -1,37 +1,37 @@
 export const products = [
   {
     id: 1,
-    src: ".boots-01.png",
+    src: "./assets/boots-01.png",
     title: "Future 7 Ultimate Football Boots",
   },
   {
     id: 2,
-    src: "jacket.png",
+    src: "./assets/jacket.png",
     title: "Future 7 Ultimate Football Boots",
   },
   {
     id: 3,
-    src: "boots-02.png",
+    src: "./assets/boots-02.png",
     title: "Future 7 Ultimate Football Boots",
   },
   {
     id: 4,
-    src: "jersey-01.png",
+    src: "./assets/jersey-01.png",
     title: "AC Milan Jersey 2023",
   },
   {
     id: 5,
-    src: "boots-03.png",
+    src: "./assets/boots-03.png",
     title: "Future 7 Ultimate Football Boots",
   },
   {
     id: 6,
-    src: "jersey-02.png",
+    src: "./assets/jersey-02.png",
     title: "Dortmund Jersey",
   },
   {
     id: 7,
-    src: "boots-04.png",
+    src: "./assets/boots-04.png",
     title: "Future 7 Ultimate Football Boots",
   },
 ];
